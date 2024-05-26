0f3772295ff45a55006548e105d7826900244406
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 00:24:01 +0200

    Add dummy function to Gallery component

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,10 @@
 import Reviews from "./Reviews"
+import Gallery from "./Gallery"
 
 function Food() {
     return ( 
         <>
+            <Gallery />
             <div>Hello from Food</div>
             <Reviews />
         </>
