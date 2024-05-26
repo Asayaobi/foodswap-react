9d68fa4dda5450f332a150189d0e4778b9d2c010
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 00:17:49 +0200

    Add dummy function to Listings component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import FoodList from './components/FoodList'
 import Bookings from './components/Bookings'
 import Food from './components/Food'
 import FoodEdit from "./components/FoodEdit"
+import Listings from "./components/Listings"
 
 function Router() {
     return (
@@ -13,6 +14,7 @@ function Router() {
             <Route path="/bookings" element={<Bookings />} />
             <Route path="/food/:id" element={<Food />} />
             <Route path="/food/:id/edit" element={<FoodEdit />} />
+            <Route path="/listings" element={<Listings />} />
 
 
         </Routes>
---
+++ src/components/Listings.jsx
@@ -0,0 +1,7 @@
+function Listings() {
+    return ( 
+    <div>Hello from Listings</div>
+    )
+}
+
+export default Listings
