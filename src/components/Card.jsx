46af74d8cd86126f1079a1dfdb5d648fab3001e2
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 23:47:32 +0200

    add layout to card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+function Card() {
+  return (
+    <>
+      <div className="grid grid-rows-2 w-80">
+        <div className=" bg-purple-500 h-80">1</div>
+        <div className=" bg-purple-400 h-28">
+          <div>Food Title</div>
+          <div>country</div>
+          <hr />
+          <div>availability</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card
