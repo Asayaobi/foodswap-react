83d0fd433b3dcc21d9ce2b415b3771ed6304e3df
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 23:16:16 +0200

    Update layout for food component

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,14 +1,31 @@
-import Reviews from "./Reviews"
-import Gallery from "./Gallery"
+import Nav from './Nav'
+import Reviews from './Reviews'
 
 function Food() {
-    return ( 
-        <>
-            <Gallery />
-            <div>Hello from Food</div>
-            <Reviews />
-        </>
-    )
+  return (
+    <>
+      <div className=" bg-slate-600 h-20">
+        Header
+        <Nav />
+      </div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2 bg-pink-200 justify-items-center">
+          {/* Gallery */}
+          <div className=" bg-pink-400 grid justify-self-end grid-rows-4">
+            <div className="row-span-3 bg-purple-500"> 1</div>
+            <div className=" bg-purple-300 grid grid-cols-3 gap-5 mt-3">
+              <div className=" bg-purple-800">a</div>
+              <div className=" bg-purple-800">a</div>
+              <div className=" bg-purple-800">a</div>
+            </div>
+          </div>
+          {/* Text */}
+          <div className=" bg-pink-600 grid justify-self-start">text</div>
+        </div>
+        <Reviews />
+      </div>
+    </>
+  )
 }
 
 export default Food
