3d159df91d9bb28a0c70a1df6fa747bcf3017786
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 22:32:47 +0200

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,29 @@
 function Nav() {
-    return (
-      <nav>
-        <button>Contact</button>
-      </nav>
-    )
-  }
-  
-  export default Nav
+  return (
+    <>
+      <div className="flex flex-col bg-blue-200">
+        <div className="bg-blue-600 h-80">
+          {/* nav bar */}
+          <div className="bg-pink-200 grid grid-cols-2 gap-10 justify-between content-start">
+            <div className="bg-pink-600">
+              <div>logo</div>
+            </div>
+            <div className="bg-blue-600 gap-10 flex justify-end">
+              <div>home</div>
+              <div>my kitchen</div>
+              <div>profile</div>
+              <div>log out</div>
+            </div>
+          </div>
+        </div>
+        {/* booking or intro */}
+        <div className="bg-blue-400 flex justify-center items-center h-80">
+          <div>pic</div>
+          <div>intro text</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Nav
