1e57bc5f80ae3d1c65d456147709f04cdb677c61
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 22:49:15 +0200

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <>
+      <div className="flex flex-col bg-blue-200">
+        <div className="bg-blue-600 h-80"></div>
+        {/* booking or intro */}
+        <div className="bg-blue-400 flex justify-center items-center h-80">
+          <div>pic</div>
+          <div>intro text</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
