34051d8aa3e54efc8ab8c2d9386fc0139df704f4
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 22:44:06 +0200

    Update Foodlist.jsx

--- src/components/Foodlist.jsx
+++ src/components/Foodlist.jsx
@@ -1,14 +1,21 @@
-import Nav from "./Nav"
-import Filter from "./Filter"
+import Nav from './Nav'
+import Filter from './Filter'
 
 function FoodList() {
-    return (
-        <>
-            <Nav />
-            <Filter />
-            <h1>Hello, from Food List!</h1>
-        </>   
-    )
-  }
-  
-  export default FoodList
+  return (
+    <>
+      <Nav />
+      <Filter />
+      <div className="container mx-auto grid grid-cols-3 justify-items-center gap-10 bg-pink-100">
+        <div className=" bg-pink-400 w-80 h-64">card1</div>
+        <div className=" bg-pink-400 w-80 h-64">card2</div>
+        <div className=" bg-pink-400 w-80 h-64">card3</div>
+        <div className=" bg-pink-400 w-80 h-64">card4</div>
+        <div className=" bg-pink-400 w-80 h-64">card5</div>
+        <div className=" bg-pink-400 w-80 h-64">card6</div>
+      </div>
+    </>
+  )
+}
+
+export default FoodList
