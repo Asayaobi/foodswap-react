a348c6c0b7bb5600ca16efe2c849662bc269e5ba
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 23:33:39 +0200

    Update layout to reviews component

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,10 @@
 function Reviews() {
-    return ( 
-    <div>Hello from Reviews</div>
-    )
+  return (
+    <div className="flex bg-blue-200 justify-between">
+      <div>reviews list</div>
+      <div className="bg-blue-500 ">leave review box</div>
+    </div>
+  )
 }
 
 export default Reviews
