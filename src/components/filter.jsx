5df8ccac8b751b5ee76f03ef05da6ec057c63bad
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 22:38:47 +0200

    Update filter.jsx

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,7 +1,14 @@
 function Filter() {
-    return ( 
-    <button>Filter</button>
-    )
+  return (
+    <>
+      <div className="flex justify-center gap-4 bg-blue-950">
+        <div className=" bg-blue-200">country</div>
+        <div className=" bg-blue-200">category</div>
+        <div className=" bg-blue-200">keyword</div>
+        <div className=" bg-blue-200">search</div>
+      </div>
+    </>
+  )
 }
 
 export default Filter
