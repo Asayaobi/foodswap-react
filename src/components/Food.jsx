bbe3ea8da48b12748ed5cd915c1f0e744a53a5e4
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-31 19:22:51 +0200

    update container to food

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -3,28 +3,24 @@ import Reviews from './Reviews'
 
 function Food() {
   return (
-    <>
-      <div className=" bg-slate-600 h-20">
-        Header
-        <Nav />
-      </div>
-      <div className="container mx-auto">
-        <div className="grid grid-cols-2 bg-pink-200 justify-items-center">
-          {/* Gallery */}
-          <div className=" bg-pink-400 grid justify-self-end grid-rows-4">
-            <div className="row-span-3 bg-purple-500"> 1</div>
-            <div className=" bg-purple-300 grid grid-cols-3 gap-5 mt-3">
-              <div className=" bg-purple-800">a</div>
-              <div className=" bg-purple-800">a</div>
-              <div className=" bg-purple-800">a</div>
-            </div>
+    <div className="container mx-auto bg-slate-600 h-20">
+      Header
+      <Nav />
+      <div className="grid grid-cols-2 bg-pink-200 justify-items-center">
+        {/* Gallery */}
+        <div className=" bg-pink-400 grid justify-self-end grid-rows-4">
+          <div className="row-span-3 bg-purple-500"> 1</div>
+          <div className=" bg-purple-300 grid grid-cols-3 gap-5 mt-3">
+            <div className=" bg-purple-800">a</div>
+            <div className=" bg-purple-800">a</div>
+            <div className=" bg-purple-800">a</div>
           </div>
-          {/* Text */}
-          <div className=" bg-pink-600 grid justify-self-start">text</div>
         </div>
-        <Reviews />
+        {/* Text */}
+        <div className=" bg-pink-600 grid justify-self-start">text</div>
       </div>
-    </>
+      <Reviews />
+    </div>
   )
 }
 
