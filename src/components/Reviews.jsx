3a1c5dd497479d53223dcb41816222210e98af8d
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-31 20:13:39 +0200

    Add dummy data to reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,25 @@
 function Reviews() {
+  const reviews = [
+    {
+      review_id: 1,
+      reviewer_id: 2,
+      food_id: 1,
+      review_text: 'Taste just like Italy',
+      rating: 5,
+      review_date: 'Jun 16, 2024',
+      author: {
+        firstname: 'Justin',
+        lastname: 'Bieber',
+        profile_image: 'https://randomuser.me/api/portraits/men/59.jpg'
+      }
+    }
+  ]
   //passing reviews list to review()
+  const reviewList = reviews.map((review, index) => (
+    <Review key={index} review={review} />
+  ))
   return (
-    <div className="grid lg:grid-cols-3 bg-pink-200 border-t-2 py-5">
+    <div className="grid lg:grid-cols-3 bg-purple-200 border-t-2 py-5">
       <div className="grid col-span-2">
         <div>number reviews</div>
         <div>number stars</div>
@@ -12,21 +30,27 @@ function Reviews() {
           <div>form for comment</div>
           <button className=" bg-gray-800 text-white">Submit</button>
         </div>
+        {/* review list */}
+        {reviewList}
       </div>
     </div>
   )
 }
 
-function Review() {
+function Review(props) {
+  const review = props.review
+  console.log('props', props)
   return (
     <div>
       <div className="flex bg-blue-600 gap-5">
-        <div>pic</div>
-        <div>firstname</div>
-        <div>lastname</div>
+        <div>
+          <img src={review.author.profile_image} alt="reviewer pic" />
+        </div>
+        <div>{review.firstname}</div>
+        <div>{review.lastname}</div>
       </div>
-      <div>rating</div>
-      <div>description</div>
+      <div>{review.rating}</div>
+      <div>{review.review_text}</div>
     </div>
   )
 }
