4619dffb3924583046c5665e571b292a07e9d2bc
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-31 23:11:03 +0200

    add data to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,35 @@
 function Header() {
   return (
     <>
-      <div className="flex flex-col bg-blue-200">
-        <div className="bg-blue-600 h-80"></div>
-        {/* booking or intro */}
-        <div className="bg-blue-400 flex justify-center items-center h-80">
-          <div>pic</div>
-          <div>intro text</div>
+      {/* booking or intro */}
+      <div className="relative h-96">
+        <div
+          className=" absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: `url(https://preview.codeless.co/picante/default/wp-content/uploads/2018/04/shop_bg-compressed.jpg)`
+          }}
+        ></div>
+        <div className=" absolute inset-0 flex items-center justify-center">
+          <img
+            src="https://i.postimg.cc/6p44gY1P/final-foodswap-001.png"
+            // or https://i.postimg.cc/6p44gY1P/final-foodswap-001.png https://i.postimg.cc/ZnqCccdV/final-option2-001.png
+            alt="logo"
+            className=" h-72"
+          />
+        </div>
+      </div>
+      <div className="flex bg-slate-100 py-10 justify-center h-192">
+        <img
+          src="https://i.ibb.co/HCw1dTJ/message-001.jpg"
+          alt="message from founder"
+          className=" w-1/2"
+        />
+        <div className="flex items-center">
+          <img
+            src="https://img.theepochtimes.com/assets/uploads/2022/06/27/sharing-food.jpg"
+            alt="sharing food"
+            className="h-96"
+          />
         </div>
       </div>
     </>
