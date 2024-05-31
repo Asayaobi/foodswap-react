cf900f9399088d60926f25b0c7aa949a405cf906
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-31 20:01:42 +0200

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,8 +1,32 @@
 function Reviews() {
+  //passing reviews list to review()
   return (
-    <div className="flex bg-blue-200 justify-between">
-      <div>reviews list</div>
-      <div className="bg-blue-500 ">leave review box</div>
+    <div className="grid lg:grid-cols-3 bg-pink-200 border-t-2 py-5">
+      <div className="grid col-span-2">
+        <div>number reviews</div>
+        <div>number stars</div>
+        <hr />
+        {/* form */}
+        <div>
+          <div>form for rating</div>
+          <div>form for comment</div>
+          <button className=" bg-gray-800 text-white">Submit</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Review() {
+  return (
+    <div>
+      <div className="flex bg-blue-600 gap-5">
+        <div>pic</div>
+        <div>firstname</div>
+        <div>lastname</div>
+      </div>
+      <div>rating</div>
+      <div>description</div>
     </div>
   )
 }
