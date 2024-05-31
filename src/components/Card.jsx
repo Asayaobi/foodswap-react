b8854c234bb6af036326973c7debefb76f780295
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-31 18:20:59 +0200

    test passing props to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,17 @@
-function Card() {
+function Card(props) {
+  const foodlist = props.food
+  console.log('props', foodlist)
   return (
     <>
       <div className="grid grid-rows-2 w-80">
         <div className=" bg-purple-500 h-80">1</div>
         <div className=" bg-purple-400 h-28">
-          <div>Food Title</div>
-          <div>country</div>
+          <div>
+            <div>{foodlist.food_title}</div>
+          </div>
+          <div>{foodlist.country}</div>
           <hr />
-          <div>availability</div>
+          <div>{foodlist.available ? 'available' : 'not available'}</div>
         </div>
       </div>
     </>
