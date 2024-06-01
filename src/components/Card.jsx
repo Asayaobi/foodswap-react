71e6c36f3a36141587ea40df5b291ebee05b4170
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-01 21:59:13 +0200

    add link to food card page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,33 @@
+import { Link } from 'react-router-dom'
+
 function Card(props) {
   const foodlist = props.food
   console.log('props', foodlist)
   return (
     <>
-      <div className="bg-white shadow-lg">
-        <div className="relative w-full pt-[100%]">
-          <img
-            src={foodlist.url}
-            alt="food pic"
-            className="absolute inset-0 w-full h-full object-cover"
-          />
-        </div>
-        <div className="p-4 text-center">
-          <div className="text-lg font-semibold">{foodlist.food_title}</div>
-          <div className="text-gray-500">{foodlist.country}</div>
-          <hr />
-          <div className="bg-orange-500 rounded-sm hover:bg-yellow-500 mt-2">
+      <Link to={`/food/${foodlist.food_id}`}>
+        <div className="bg-white shadow-lg hover:bg-slate-100">
+          <div className="relative w-full pt-[100%]">
+            <img
+              src={foodlist.url}
+              alt="food pic"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+          </div>
+          <div className="p-4 text-center">
+            <div className="text-lg font-semibold">{foodlist.food_title}</div>
+            <div className="text-gray-500">{foodlist.country}</div>
+            <hr />
             {foodlist.available ? (
-              <button className=" bg-orange-500 hover:bg-yellow-500 text-white">
+              <button className=" bg-orange-500 hover:bg-yellow-500 w-full text-white mt-2">
                 ready to swap
               </button>
             ) : (
-              <div className=" text-orange-500">not available</div>
+              <div className=" text-gray-500">not available</div>
             )}
           </div>
         </div>
-      </div>
+      </Link>
     </>
   )
 }
