92e60c8a42cda5ccf00d2770e1ad82c05a082d92
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-01 22:40:04 +0200

    Add dummy data to Nav

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,29 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import FoodList from "./components/FoodList"
-import Bookings from "./components/Bookings"
-import Food from "./components/Food"
-import FoodEdit from "./components/FoodEdit"
-import Listings from "./components/Listings"
-import LogIn from "./components/LogIn"
-import Profile from "./components/Profile"
-import SignUp from "./components/SignUp"
+import FoodList from './components/FoodList'
+import Bookings from './components/Bookings'
+import Food from './components/Food'
+import FoodEdit from './components/FoodEdit'
+import Listings from './components/Listings'
+import LogIn from './components/LogIn'
+import Profile from './components/Profile'
+import SignUp from './components/SignUp'
 
 function Router() {
-    return (
-        <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<FoodList />} />
-            <Route path="/bookings" element={<Bookings />} />
-            <Route path="/food/:id" element={<Food />} />
-            <Route path="/food/:id/edit" element={<FoodEdit />} />
-            <Route path="/listings" element={<Listings />} />
-            <Route path="/login" element={<LogIn />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/signup" element={<SignUp />} />
-        </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FoodList />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/food/:id" element={<Food />} />
+        <Route path="/food/:id/edit" element={<FoodEdit />} />
+        <Route path="/listings" element={<Listings />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default Router
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,38 @@
+import { Link } from 'react-router-dom'
+
 function Nav() {
   return (
     <>
-      <div className="bg-pink-200 grid grid-cols-2 gap-10 justify-between content-start">
-        <div className="bg-pink-600">
-          <div>logo</div>
-        </div>
-        <div className="bg-blue-600 gap-10 flex justify-end">
-          <div>home</div>
-          <div>my kitchen</div>
-          <div>profile</div>
-          <div>log out</div>
+      <div className="absolute inset-0 grid grid-cols-2 gap-10 justify-between content-start">
+        <Link to="/">
+          <img
+            src="https://i.postimg.cc/bYChMmRP/triangle.png"
+            alt="foodswap logo"
+            className="pl-5 h-40"
+          />
+        </Link>
+
+        <div className="gap-10 flex justify-end">
+          <Link to="/">
+            <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:text-orange-200">
+              Home
+            </button>
+          </Link>
+          <Link to="/listings">
+            <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:text-orange-200">
+              My Kitchen
+            </button>
+          </Link>
+          <Link to="/profile">
+            <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:text-orange-200">
+              My Profile
+            </button>
+          </Link>
+          <Link to="/profile">
+            <button className="h-10 items-center border border-white font-bold text-white text-xs md:text-base my-10 px-8  hover:text-orange-200">
+              Log Out
+            </button>
+          </Link>
         </div>
       </div>
     </>
