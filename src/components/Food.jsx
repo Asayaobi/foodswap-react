db2c1b379bafcfd2a480a40887097c5e38012a63
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-01 23:55:00 +0200

    Add dummy data to food

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -2,22 +2,104 @@ import Nav from './Nav'
 import Reviews from './Reviews'
 
 function Food() {
+  const food = {
+    food_id: 2,
+    food_title: 'Tonkatsu Ramen',
+    country: 'Japan',
+    category: 'main dish',
+    ingredients:
+      'egg noodle, pork, seaweed, green onion, mushroom, soy sause, tare seasoning.',
+    description: `This ramen features a rich, savory broth made from pork bones. The name "tonkotsu" literally means "pork bones" in Japanese. The broth gets its deep, flavorful taste and creamy, cloudy appearance from boiling the pork bones for up to eighteen hours. This slow cooking process results in a hearty, satisfying bowl of ramen that's sure to warm you up and leave you craving more.`,
+    chef_id: 2,
+    rating: 3,
+    available: true,
+    chef: {
+      user_id: 2,
+      profile_image: 'url',
+      firstname: 'Justin',
+      lastname: 'Bieber'
+    },
+    images: [
+      'https://i1.wp.com/seonkyounglongest.com/wp-content/uploads/2020/09/Easy-Tonkotsu-26-mini.jpg',
+      'https://i0.wp.com/www.angsarap.net/wp-content/uploads/2022/03/Tonkotsu-Ramen-Wide.jpg',
+      'https://www.seriouseats.com/thmb/IBikLAGkkP2QVaF3vLIk_LeNqHM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/rich-and-creamy-tonkotsu-ramen-broth-from-scratch-recipe-Diana-Chistruga-hero-6d318fadcca64cc9ac3e1c40fc7682fb.JPG',
+      'https://www.seriouseats.com/thmb/GNFUxllntjgtfQiAd6lofC72JjY=/500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__images__20120227-tonkotsu-ramen-broth-pork-fat-24-1451c421c7d74cc08b0c2b3e26f1ec8f.jpg'
+    ]
+  }
   return (
-    <div className="container mx-auto bg-slate-600 h-20">
-      Header
+    <div className="container mx-auto">
+      <div
+        className="h-40 bg-center"
+        style={{
+          backgroundImage: `url(https://preview.codeless.co/picante/default/wp-content/uploads/2018/05/top_shop.jpg)`
+        }}
+      ></div>
       <Nav />
-      <div className="grid grid-cols-2 bg-pink-200 justify-items-center">
-        {/* Gallery */}
-        <div className=" bg-pink-400 grid justify-self-end grid-rows-4">
-          <div className="row-span-3 bg-purple-500"> 1</div>
-          <div className=" bg-purple-300 grid grid-cols-3 gap-5 mt-3">
-            <div className=" bg-purple-800">a</div>
-            <div className=" bg-purple-800">a</div>
-            <div className=" bg-purple-800">a</div>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-2 my-10 mx-36 w-2/3 justify-start">
+          {/* Gallery */}
+          <div className=" grid grid-rows-4 gap-2">
+            <div className="grid row-span-3 aspect-square">
+              <img
+                src={food.images[0]}
+                alt="food pic"
+                className=" w-full h-full object-cover"
+              />
+            </div>
+            <div className=" grid grid-cols-3 gap-5 mt-3">
+              <div>
+                <img
+                  src={food.images[1]}
+                  alt="food pic"
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+              <div>
+                <img
+                  src={food.images[2]}
+                  alt="food pic"
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+              <div>
+                <img
+                  src={food.images[3]}
+                  alt="food pic"
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+          {/* Text */}
+          <div className="grid grid-rows-5 items-center pl-4">
+            <div className="grid row-span-2 gap-4">
+              <div className=" text-4xl font-serif font-extrabold">
+                {food.food_title}
+              </div>
+              <div className=" text-2xl font-serif font-extrabold text-slate-400">
+                {food.country}
+              </div>
+              <div className=" pb-3">{food.description}</div>
+              <hr />
+            </div>
+            <div className="grid row-span-2 gap-4">
+              <div className=" text-2xl font-serif font-extrabold">
+                INGREDIENTS
+              </div>
+              <div>{food.ingredients}</div>
+              <div className=" text-2xl font-serif font-extrabold">
+                CATEGORIES
+              </div>
+              <div className="pb-3">{food.category}</div>
+              <hr />
+            </div>
+            <div className=" text-center">
+              <button className=" bg-yellow-500 text-white w-full hover:bg-yellow-700">
+                S W A P
+              </button>
+            </div>
           </div>
         </div>
-        {/* Text */}
-        <div className=" bg-pink-600 grid justify-self-start">text</div>
       </div>
       <Reviews />
     </div>
