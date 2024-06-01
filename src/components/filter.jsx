fc15871158beb1827c4361670f29fc811ba00b74
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-01 21:49:03 +0200

    add layout to filter

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,11 +1,32 @@
 function Filter() {
   return (
     <>
-      <div className="flex justify-center gap-4 bg-blue-950">
-        <div className=" bg-blue-200">country</div>
-        <div className=" bg-blue-200">category</div>
-        <div className=" bg-blue-200">keyword</div>
-        <div className=" bg-blue-200">search</div>
+      <div className="flex justify-center mt-10 gap-4 mx-48">
+        {/* location */}
+        <select
+          name="location"
+          className="border border-gray-300 rounded ps-9 w-full p-2"
+        >
+          <option value="">Any Location</option>
+        </select>
+        {/* category */}
+        <select
+          name="category"
+          className="border border-gray-300 rounded ps-9 w-full p-2"
+        >
+          <option value="">All category</option>
+        </select>
+        {/* keyword */}
+        <input
+          name="keyword"
+          className="border border-gray-300 rounded ps-9 w-full p-2"
+          type="text"
+          placeholder="keywords..."
+        />
+        {/* button */}
+        <button className="flex relative bg-slate-100 items-center p-5 border h-11 sm:w-full lg:w-40">
+          Search
+        </button>
       </div>
     </>
   )
