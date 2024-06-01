142a14c9fab31bf099ead64cb3d25431c18c649d
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-01 18:32:04 +0200

    update layout to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,27 @@ function Card(props) {
   console.log('props', foodlist)
   return (
     <>
-      <div className="grid grid-rows-2 w-80">
-        <div className=" bg-purple-500 h-80">1</div>
-        <div className=" bg-purple-400 h-28">
-          <div>
-            <div>{foodlist.food_title}</div>
-          </div>
-          <div>{foodlist.country}</div>
+      <div className="bg-white shadow-lg">
+        <div className="relative w-full pt-[100%]">
+          <img
+            src={foodlist.url}
+            alt="food pic"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+        <div className="p-4 text-center">
+          <div className="text-lg font-semibold">{foodlist.food_title}</div>
+          <div className="text-gray-500">{foodlist.country}</div>
           <hr />
-          <div>{foodlist.available ? 'available' : 'not available'}</div>
+          <div className="bg-orange-500 rounded-sm hover:bg-yellow-500 mt-2">
+            {foodlist.available ? (
+              <button className=" bg-orange-500 hover:bg-yellow-500 text-white">
+                ready to swap
+              </button>
+            ) : (
+              <div className=" text-orange-500">not available</div>
+            )}
+          </div>
         </div>
       </div>
     </>
