22d457ee2a7f426c3d454f6e97ed35836cefeb6e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-03 00:34:03 +0200

    Update link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom'
+
 function Footer() {
   return (
     <>
-      <div className=" bg-slate-800 h-20 flex justify-center items-center text-slate-400 text-sm">
-        © 2024 FOOD SWAP PROJECT. ALL RIGHTS RESERVED
+      <div className=" bg-slate-800 h-20 flex justify-center items-center text-slate-400 text-sm gap-2">
+        <div>© 2024 FOOD SWAP PROJECT BY </div>
+        <Link to="https://portfolio-asaya.onrender.com">
+          <div>ASAYAOBI</div>
+        </Link>
+
+        <div> ▲ ALL RIGHTS RESERVED</div>
       </div>
     </>
   )
