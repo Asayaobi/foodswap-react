019424f4bfca32e8a0695b97aa4bb5e00578306e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-02 17:53:16 +0200

    add dummy data and layout to reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,31 +4,61 @@ function Reviews() {
       review_id: 1,
       reviewer_id: 2,
       food_id: 1,
-      review_text: 'Taste just like Italy',
-      rating: 5,
+      review_text: 'Taste just like Japan',
+      rating: 4,
       review_date: 'Jun 16, 2024',
       author: {
         firstname: 'Justin',
-        lastname: 'Bieber',
-        profile_image: 'https://randomuser.me/api/portraits/men/59.jpg'
+        lastname: 'Brown',
+        profile_image: 'https://randomuser.me/api/portraits/men/19.jpg'
+      }
+    },
+    {
+      review_id: 2,
+      reviewer_id: 2,
+      food_id: 1,
+      review_text: 'Authentic!',
+      rating: 3,
+      review_date: 'Jun 17, 2024',
+      author: {
+        firstname: 'Sammy',
+        lastname: 'Powel',
+        profile_image: 'https://randomuser.me/api/portraits/men/9.jpg'
       }
     }
   ]
   //passing reviews list to review()
-  const reviewList = reviews.map((review, index) => (
-    <Review key={index} review={review} />
+  const reviewList = reviews.map((review) => (
+    <Review key={review.review_id} review={review} />
   ))
   return (
-    <div className="grid lg:grid-cols-3 bg-purple-200 border-t-2 py-5">
-      <div className="grid col-span-2">
-        <div>number reviews</div>
-        <div>number stars</div>
+    <div className="mx-44">
+      <div className="grid px-20 py-10 gap-4">
+        {/* count the total reviews */}
+        <div className="underline underline-offset-[70px] text-2xl font-serif font-bold">
+          REVIEWS ({reviews.length})
+        </div>
+        {/* calculate the average rating */}
+        <div className="text-2xl font-serif font-bold">
+          RATING{' '}
+          {Math.ceil(
+            reviews.reduce((sum, review) => sum + review.rating, 0) /
+              reviews.length
+          )}
+        </div>
         <hr />
         {/* form */}
-        <div>
-          <div>form for rating</div>
-          <div>form for comment</div>
-          <button className=" bg-gray-800 text-white">Submit</button>
+        <div className=" my-10 space-y-4">
+          <div className="text-2xl font-serif font-bold">Leave a comment</div>
+          <div>Taste Rating</div>
+          <div>Comment</div>
+          <textarea
+            name="review_text"
+            className=" w-full text-sm pb-16 pr-16 bg-slate-100"
+          ></textarea>
+          <button className=" bg-slate-800 text-white py-1 px-4 tracking-widest">
+            POST COMMENT
+          </button>
         </div>
         {/* review list */}
         {reviewList}
@@ -37,20 +67,29 @@ function Reviews() {
   )
 }
 
-function Review(props) {
-  const review = props.review
-  console.log('props', props)
+function Review({ review }) {
   return (
     <div>
-      <div className="flex bg-blue-600 gap-5">
+      <div className="flex gap-5">
         <div>
-          <img src={review.author.profile_image} alt="reviewer pic" />
+          <img
+            src={review.author.profile_image}
+            alt="reviewer pic"
+            className="rounded-full w-2/3"
+          />
         </div>
-        <div>{review.firstname}</div>
-        <div>{review.lastname}</div>
+        <div className=" space-y-4">
+          <div className="flex pt-3 gap-2 text-xl font-bold">
+            <div>{review.author.firstname}</div>
+            <div>{review.author.lastname}</div>
+          </div>
+          <div className=" text-slate-300 text-sm">{review.review_date}</div>
+          <div className=" space-y-4">
+            <div>{review.rating}</div>
+            <div className=" pb-8">{review.review_text}</div>
+          </div>
+        </div>
       </div>
-      <div>{review.rating}</div>
-      <div>{review.review_text}</div>
     </div>
   )
 }
