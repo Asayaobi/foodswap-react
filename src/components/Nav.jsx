9a0a8d0071ea8a175cc87d313939281620be1883
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-03 00:02:12 +0200

    update layout to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Nav() {
   return (
     <div>
-      <div className="absolute inset-0 grid grid-cols-2 gap-10 justify-between content-start mr-28 ml-14">
+      <div className="absolute inset-0 grid grid-cols-2 gap-10 justify-between content-start mr-20 ml-14">
         <Link to="/">
           <img
             src="https://i.postimg.cc/MphWSVpT/whitetrianglelogo-001.png"
@@ -14,22 +14,22 @@ function Nav() {
 
         <div className="gap-10 flex justify-end">
           <Link to="/">
-            <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:text-orange-200">
+            <button className="px-8 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:border border-white">
               Home
             </button>
           </Link>
           <Link to="/listings">
-            <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:text-orange-200">
+            <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:border border-white">
               My Kitchen
             </button>
           </Link>
           <Link to="/profile">
-            <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:text-orange-200">
+            <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:border border-white">
               My Profile
             </button>
           </Link>
-          <Link to="/profile">
-            <button className="h-10 items-center border border-white font-bold text-white text-xs md:text-base my-10 px-8  hover:text-orange-200">
+          <Link to="/">
+            <button className="h-10 items-center font-bold text-white text-xs md:text-base my-10 px-8  hover:border border-white">
               Log Out
             </button>
           </Link>
