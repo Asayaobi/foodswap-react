183012aa7393d0269404b5b799d5f3014b327430
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-02 18:04:54 +0200

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <div className=" bg-slate-800 h-20 flex justify-center items-center text-slate-400 text-sm">
+        © 2024 FOOD SWAP PROJECT. ALL RIGHTS RESERVED
+      </div>
+    </>
+  )
+}
+
+export default Footer
