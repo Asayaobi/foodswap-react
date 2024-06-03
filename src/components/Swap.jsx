5444175c777b9afb3ea70b1f82f4889761c71fd2
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-03 23:44:18 +0200

    Create form to swap component

---
+++ src/components/Swap.jsx
@@ -0,0 +1,13 @@
+function Swap() {
+  return (
+    <div className=" text-center">
+      <form>
+        <button className=" bg-yellow-500 text-white w-full hover:bg-yellow-700 tracking-widest py-5 rounded-sm">
+          REQUEST TO SWAP
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Swap
