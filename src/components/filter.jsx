e532e5c560c937e1f12ce710b906c71786ac3340
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-03 23:35:43 +0200

    Add form to Filter

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -2,38 +2,40 @@ function Filter() {
   return (
     <>
       <div className="bg-slate-50 flex justify-center gap-4 px-48">
-        {/* location */}
-        <select
-          name="location"
-          className="border border-gray-300 rounded ps-9 w-full p-2"
-        >
-          <option value="">Any Location</option>
-        </select>
-        {/* country */}
-        <select
-          name="country"
-          className="border border-gray-300 rounded ps-9 w-full p-2"
-        >
-          <option value="">Cuisine Type</option>
-        </select>
-        {/* category */}
-        <select
-          name="category"
-          className="border border-gray-300 rounded ps-9 w-full p-2"
-        >
-          <option value="">Course Category</option>
-        </select>
-        {/* keyword */}
-        <input
-          name="keyword"
-          className="border border-gray-300 rounded ps-9 w-full p-2"
-          type="text"
-          placeholder="keywords..."
-        />
-        {/* button */}
-        <button className="flex relative bg-slate-100 items-center p-5 border h-11 sm:w-full lg:w-40">
-          Search
-        </button>
+        <form>
+          {/* location */}
+          <select
+            name="location"
+            className="border border-gray-300 rounded ps-9 w-full p-2"
+          >
+            <option value="">Any Location</option>
+          </select>
+          {/* country */}
+          <select
+            name="country"
+            className="border border-gray-300 rounded ps-9 w-full p-2"
+          >
+            <option value="">Cuisine Type</option>
+          </select>
+          {/* category */}
+          <select
+            name="category"
+            className="border border-gray-300 rounded ps-9 w-full p-2"
+          >
+            <option value="">Course Category</option>
+          </select>
+          {/* keyword */}
+          <input
+            name="keyword"
+            className="border border-gray-300 rounded ps-9 w-full p-2"
+            type="text"
+            placeholder="keywords..."
+          />
+          {/* button */}
+          <button className="flex relative bg-slate-100 items-center p-5 border h-11 sm:w-full lg:w-40">
+            Search
+          </button>
+        </form>
       </div>
     </>
   )
