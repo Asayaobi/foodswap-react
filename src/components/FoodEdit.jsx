cfc6e32e5fdb1535960898bbaa51a9306ca66ce4
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-04 00:35:26 +0200

    Update Form in FoodEdit

--- src/components/FoodEdit.jsx
+++ src/components/FoodEdit.jsx
@@ -25,8 +25,9 @@ function FoodEdit() {
     <>
       <div className="relative h-screen">
         <div className="absolute inset-0 bg-[url('https://preview.codeless.co/picante/default/wp-content/uploads/2018/07/home02_03-compressed.jpg')] bg-cover bg-center"></div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="bg-slate-100 p-8 opacity-90 rounded shadow-lg">
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <Nav />
+          <div className="bg-slate-100 p-8 opacity-90 rounded shadow-lg max-w-4xl w-full">
             <h1 className="text-2xl font-bold mb-8 font-serif">
               Edit this Dish
             </h1>
@@ -34,7 +35,7 @@ function FoodEdit() {
               <div className="grid grid-cols-3 gap-8">
                 <div>
                   <div className="mb-4">
-                    <label className="block">Dish Name</label>
+                    <label>Dish Name</label>
                     <input
                       type="text"
                       name="food_title"
@@ -42,7 +43,7 @@ function FoodEdit() {
                     />
                   </div>
                   <div className="mb-4">
-                    <label className="block">Culinary Origin</label>
+                    <label>Culinary Origin</label>
                     <input
                       type="text"
                       name="country"
@@ -50,7 +51,7 @@ function FoodEdit() {
                     />
                   </div>
                   <div className="mb-4">
-                    <label className="block">Course Category</label>
+                    <label>Course Category</label>
                     <input
                       type="text"
                       name="category"
@@ -58,7 +59,7 @@ function FoodEdit() {
                     />
                   </div>
                   <div className="mb-4">
-                    <label className="block">Ingredients</label>
+                    <label>Ingredients</label>
                     <input
                       type="text"
                       name="ingredients"
@@ -66,7 +67,7 @@ function FoodEdit() {
                     />
                   </div>
                   <div className="mb-4">
-                    <label className="block">Availability</label>
+                    <label>Availability</label>
                     <select
                       name="availability"
                       className="border rounded w-full py-2 px-3"
@@ -77,7 +78,7 @@ function FoodEdit() {
                   </div>
                 </div>
                 <div className="col-span-2">
-                  <label className="block">Description</label>
+                  <label>Description</label>
                   <input
                     type="text"
                     name="description"
@@ -101,7 +102,7 @@ function FoodEdit() {
                     className="border rounded py-2 px-3 w-full mb-2"
                   />
                   <div className="flex justify-end">
-                    <button className="bg-orange-500 hover:bg-orange-300 text-white mt-2 py-3 px-8 tracking-widest">
+                    <button className="bg-orange-500 hover:bg-orange-300 text-white mt-2 py-3 px-8 tracking-widest rounded-sm">
                       EDIT THIS DISH
                     </button>
                   </div>
@@ -110,7 +111,6 @@ function FoodEdit() {
             </form>
           </div>
         </div>
-        <Nav />
       </div>
       {/* Cards Session */}
       <div className=" text-center bg-slate-100 py-4">
