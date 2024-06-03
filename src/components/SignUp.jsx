7720f5c135b3aa82bc0bee2baa2a13f874048d8e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-03 23:51:35 +0200

    Add form to Sign Up component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -22,54 +22,64 @@ function SignUp() {
             </Link>
           </div>
           {/* Log In Form */}
-          <div className="py-3">Name</div>
-          <div className=" grid grid-cols-3 gap-3">
+          <form>
+            <div className="py-3">Name</div>
+            <div className=" grid grid-cols-3 gap-3">
+              <input
+                type="text"
+                name="firstname"
+                placeholder="first name"
+                className="border p-1 rounded"
+                autoFocus
+              />
+              <input
+                type="text"
+                name="lastname"
+                placeholder="last name"
+                className="border p-1 rounded grid col-span-2"
+                autoFocus
+              />
+            </div>
+            <div className="py-3">
+              <label>Email</label>
+            </div>
             <input
-              type="text"
-              name="firstname"
-              placeholder="first name"
-              className="border p-1 rounded"
+              type="email"
+              name="email"
+              className="border w-full p-1 rounded"
               autoFocus
             />
+            <div className="py-3">
+              <label>Password</label>
+            </div>
             <input
-              type="text"
-              name="lastname"
-              placeholder="last name"
-              className="border p-1 rounded grid col-span-2"
+              type="password"
+              name="password"
+              className="border w-full p-1 rounded"
               autoFocus
             />
-          </div>
-          <div className="py-3">Email</div>
-          <input
-            type="email"
-            name="email"
-            className="border w-full p-1 rounded"
-            autoFocus
-          />
-          <div className="py-3">Password</div>
-          <input
-            type="password"
-            name="password"
-            className="border w-full p-1 rounded"
-            autoFocus
-          />
-          <div className="py-3">Profile Picture</div>
-          <input
-            type="text"
-            name="profile_image"
-            className="border w-full p-1 rounded"
-            autoFocus
-          />
-          <div className="py-3">City</div>
-          <input
-            type="text"
-            name="city"
-            className="border w-full p-1 rounded mb-8"
-            autoFocus
-          />
-          <button className=" bg-orange-500 hover:bg-orange-300 w-full text-white mt-2 py-3">
-            Register
-          </button>
+            <div className="py-3">
+              <label>Profile Picture</label>
+            </div>
+            <input
+              type="text"
+              name="profile_image"
+              className="border w-full p-1 rounded"
+              autoFocus
+            />
+            <div className="py-3">
+              <label>City</label>
+            </div>
+            <input
+              type="text"
+              name="city"
+              className="border w-full p-1 rounded mb-8"
+              autoFocus
+            />
+            <button className=" bg-orange-500 hover:bg-orange-300 w-full text-white mt-2 py-3 rounded-md">
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
