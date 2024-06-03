1958592edf88f6cca4f976790cb70b23eec91d29
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-03 19:48:43 +0200

    update display button in card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom'
 function Card(props) {
   const foodlist = props.food
   const isListing = props.isListing
+  const isBooking = props.isBooking
   console.log('props', foodlist)
+  console.log('props booking', isBooking)
+  console.log('listing', isListing)
   return (
     <>
       <Link to={`/food/${foodlist.food_id}`}>
@@ -20,6 +23,12 @@ function Card(props) {
             <div className="text-gray-500">{foodlist.country}</div>
             <hr />
             {/* button */}
+            {!isListing && !isBooking ? (
+              <button className=" bg-orange-500 hover:bg-yellow-500 w-full text-white mt-2">
+                ready to swap
+              </button>
+            ) : null}
+            {/* From Listing */}
             {isListing ? (
               <div className="flex justify-between px-2 gap-1 mt-3 ml-1">
                 <Link to={`/food/${foodlist.food_id}`}>
@@ -33,11 +42,23 @@ function Card(props) {
                   </button>
                 </Link>
               </div>
-            ) : (
-              <button className=" bg-orange-500 hover:bg-yellow-500 w-full text-white mt-2">
-                ready to swap
-              </button>
-            )}
+            ) : null}
+            {/* From Booking */}
+            {isBooking ? (
+              foodlist.swap ? (
+                <Link to={`/food/${foodlist.food_id}`}>
+                  <div className=" bg-slate-700 px-10 text-white hover:bg-slate-500">
+                    Swapped
+                  </div>
+                </Link>
+              ) : (
+                <Link to={`/food/${foodlist.food_id}`}>
+                  <div className=" bg-slate-700 px-10 text-white hover:bg-slate-500">
+                    Pending
+                  </div>
+                </Link>
+              )
+            ) : null}
           </div>
         </div>
       </Link>
