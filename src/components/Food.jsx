776e9fe55f88dfcbf0a3b8bfe0cfd9d4124439b8
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-08 18:59:59 +0200

    update layout in Food component

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -33,17 +33,17 @@ function Food() {
       ></div>
       <Nav />
       <div className="flex justify-center">
-        <div className="flex justify-start space-x-5 mx-32 my-28">
+        <div className="grid grid-cols-2 items-center place-items-center space-x-5 mx-40 my-28 ">
           {/* Gallery */}
-          <div className=" grid grid-rows-4 gap-2 w-2/3">
-            <div className="grid row-span-3 aspect-square">
+          <div className=" grid grid-rows-3 gap-2 place-items-start">
+            <div className="grid row-span-2 aspect-square">
               <img
                 src={selectedImage}
                 alt="food pic"
                 className=" w-full h-full object-cover"
               />
             </div>
-            <div className=" grid grid-cols-3 gap-2 mt-3">
+            <div className=" grid grid-cols-3 gap-2 mt-3 ">
               {food.images &&
                 food.images.slice(1).map((image, index) => (
                   <div key={index}>
@@ -60,7 +60,7 @@ function Food() {
             </div>
           </div>
           {/* Text */}
-          <div className="grid grid-rows-5 items-center w-2/3">
+          <div className="grid grid-rows-5 items-center ">
             <div className="grid row-span-2 gap-4">
               <div className=" text-4xl font-serif font-extrabold">
                 {food.food_title}
