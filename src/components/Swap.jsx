fd346f951c6e0204ebecbd20134c9c49bd8a9128
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-10 18:12:40 +0200

    add submit form function to swap component

--- src/components/Swap.jsx
+++ src/components/Swap.jsx
@@ -1,8 +1,25 @@
+import axios from 'axios'
+axios.defaults.withCredentials = true
+
 function Swap({ foodId }) {
   console.log('foodId', foodId)
+  const formObject = {
+    food_id: foodId
+  }
+  const submitForm = async (e) => {
+    try {
+      e.preventDefault()
+      const { data } = await axios.post(
+        'http://localhost:4000//bookings',
+        formObject
+      )
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <div className=" text-center px-4">
-      <form>
+      <form onSubmit={(e) => submitForm(e)}>
         <button className=" bg-yellow-500 text-white w-full hover:bg-yellow-700 tracking-widest py-5 rounded-md">
           REQUEST TO SWAP
         </button>
