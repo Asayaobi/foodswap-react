204f2f08f0d16c81c1efbfe55af47a0110af429e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-24 01:27:39 +0200

    update listing cards in listing component

--- src/components/Foodlist.jsx
+++ src/components/Foodlist.jsx
@@ -8,6 +8,7 @@ import { useState, useEffect } from 'react'
 
 function FoodList() {
   const [foodlist, setFoodList] = useState([])
+  const isLoggedIn = localStorage.getItem('isLoggedIn')
 
   const getFood = async () => {
     let { data } = await axios.get('http://localhost:4000/food')
@@ -17,9 +18,7 @@ function FoodList() {
   useEffect(() => {
     getFood()
   }, [])
-  const listOfCards = foodlist.map((food, index) => (
-    <Card key={index} food={food} />
-  ))
+
   return (
     <div className="container mx-auto">
       <Header />
@@ -27,7 +26,12 @@ function FoodList() {
       <Filter setFoodList={setFoodList} />
       <div className="bg-slate-50 grid justify-stretch pb-28 pt-10 px-48">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-          {listOfCards}
+          {/* Review only 6 cards if the user is not logged in */}
+          {isLoggedIn
+            ? foodlist.map((food, index) => <Card key={index} food={food} />)
+            : foodlist
+                .slice(0, 6)
+                .map((food, index) => <Card key={index} food={food} />)}
         </div>
       </div>
       <div>
