5415cb240beea0004d3669a6c2492fca4edb2f71
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-30 20:15:34 +0200

    Add logo to sign in

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -50,9 +50,9 @@ function LogIn() {
         <div className=" bg-white px-48 py-40 pt-14">
           <Link to="/">
             <img
-              src="https://i.postimg.cc/L6W3VbhW/foodswapblacklogotriangle-002.png"
+              src="https://i.postimg.cc/QNybcRwy/cleaned-inline-orange-logo-with-black-text.png"
               alt="foodswap logo"
-              className="pl-5 h-40"
+              className="h-40"
             />
           </Link>
           {/* Log In Form */}
