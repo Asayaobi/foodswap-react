05aaa5e8a56a6c675b0a9b72b7694b43e6e4944a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-30 19:27:59 +0200

    Update loggedin function to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,46 +7,76 @@ function Footer() {
 
   return (
     <>
-      <div
-        className="relative h-80 grid place-items-center justify-center items-center gap-2"
-        style={{
-          backgroundImage: `url(https://i.postimg.cc/3wJTSmyL/dietary-salad-with-mussels-quail-eggs-cucumbers-radish-lettuce-healthy-food-seafood-salad-top-view-f.jpg)`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat'
-        }}
-      >
-        <div className="absolute inset-0 bg-slate-800 opacity-60"></div>
-        {/* Delivery Text box */}
-        <div className="gap-2">
-          <div className="relative text-white text-3xl font-serif tracking-wide">
-            Fast, Fresh, and Fabulous - That's Our Promise!
-          </div>
-          <div className="flex text-center gap-2 items-center justify-center">
-            <FontAwesomeIcon
-              icon={faPhone}
-              className="relative text-white text-lg pt-2"
-            />
-            <div className="relative text-center text-white text-3xl font-serif tracking-wide">
-              1122
+      {isLoggedIn ? (
+        <div
+          className="relative h-20 grid place-items-center gap-2"
+          style={{
+            backgroundImage: `url(https://i.postimg.cc/3wJTSmyL/dietary-salad-with-mussels-quail-eggs-cucumbers-radish-lettuce-healthy-food-seafood-salad-top-view-f.jpg)`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+          }}
+        >
+          <div className="absolute inset-0 bg-slate-800 opacity-60"></div>
+
+          {/* Footer Text */}
+          <div className="flex">
+            <div className="relative text-slate-300">
+              © 2024 FOOD SWAP PROJECT BY
+            </div>
+            <Link to="https://portfolio-asaya.onrender.com">
+              <div className="relative text-slate-50 px-2">ASAYAOBI</div>
+            </Link>
+            <div className="relative text-slate-300">
+              {' '}
+              ▲ ALL RIGHTS RESERVED
             </div>
           </div>
-          <div className="flex justify-center pt-4">
-            <button className="relative py-2 px-5 font-serif text-white text-xl border border-white hover:border-orange-400">
-              <Link to="/login">Order Now</Link>
-            </button>
+        </div>
+      ) : (
+        <div
+          className="relative h-80 grid place-items-center gap-2"
+          style={{
+            backgroundImage: `url(https://i.postimg.cc/3wJTSmyL/dietary-salad-with-mussels-quail-eggs-cucumbers-radish-lettuce-healthy-food-seafood-salad-top-view-f.jpg)`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+          }}
+        >
+          <div className="absolute inset-0 bg-slate-800 opacity-60"></div>
+          {/* Delivery Text box */}
+          <div className="gap-2">
+            <div className="relative text-white text-3xl font-serif tracking-wide">
+              Fast, Fresh, and Fabulous - That's Our Promise!
+            </div>
+            <div className="flex text-center gap-2 items-center justify-center">
+              <FontAwesomeIcon
+                icon={faPhone}
+                className="relative text-white text-lg pt-2"
+              />
+              <div className="relative text-center text-white text-3xl font-serif tracking-wide">
+                1122
+              </div>
+            </div>
+            <div className="flex justify-center pt-4">
+              <button className="relative py-2 px-5 font-serif text-white text-xl border border-white hover:border-orange-400">
+                <Link to="/login">Order Now</Link>
+              </button>
+            </div>
+          </div>
+          {/* Footer Text */}
+          <div className="flex">
+            <div className="relative text-slate-300">
+              © 2024 FOOD SWAP PROJECT BY
+            </div>
+            <Link to="https://portfolio-asaya.onrender.com">
+              <div className="relative text-slate-50 px-2">ASAYAOBI</div>
+            </Link>
+            <div className="relative text-slate-300">
+              {' '}
+              ▲ ALL RIGHTS RESERVED
+            </div>
           </div>
         </div>
-        {/* Footer Text */}
-        <div className="flex">
-          <div className="relative text-slate-300">
-            © 2024 FOOD SWAP PROJECT BY
-          </div>
-          <Link to="https://portfolio-asaya.onrender.com">
-            <div className="relative text-slate-50 px-2">ASAYAOBI</div>
-          </Link>
-          <div className="relative text-slate-300"> ▲ ALL RIGHTS RESERVED</div>
-        </div>
-      </div>
+      )}
     </>
   )
 }
