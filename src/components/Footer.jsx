95fe96b3597225ec2d9af9e96f2584403055ba9d
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-06-30 18:49:05 +0200

    Add footer background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,36 @@
 import { Link } from 'react-router-dom'
 
 function Footer() {
+  const isLoggedIn = localStorage.getItem('isLoggedIn')
+  console.log(isLoggedIn)
+
   return (
     <>
-      <div className=" bg-slate-800 h-20 flex justify-center items-center text-slate-400 text-sm gap-2">
-        <div>© 2024 FOOD SWAP PROJECT BY </div>
-        <Link to="https://portfolio-asaya.onrender.com">
-          <div>ASAYAOBI</div>
-        </Link>
-
-        <div> ▲ ALL RIGHTS RESERVED</div>
+      <div
+        className="relative h-80 grid place-items-center justify-center items-center gap-2"
+        style={{
+          backgroundImage: `url(https://i.postimg.cc/3wJTSmyL/dietary-salad-with-mussels-quail-eggs-cucumbers-radish-lettuce-healthy-food-seafood-salad-top-view-f.jpg)`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat'
+        }}
+      >
+        <div className="absolute inset-0 bg-slate-800 opacity-60"></div>
+        {/* Delivery Text box */}
+        <div>
+          <div className="relative text-white text-lg">
+            Fast, Fresh, and Fabulous - That's Our Promise!
+          </div>
+        </div>
+        {/* Footer Text */}
+        <div className="flex">
+          <div className="relative text-slate-300">
+            © 2024 FOOD SWAP PROJECT BY
+          </div>
+          <Link to="https://portfolio-asaya.onrender.com">
+            <div className="relative text-slate-50">ASAYAOBI</div>
+          </Link>
+          <div className="relative text-slate-300"> ▲ ALL RIGHTS RESERVED</div>
+        </div>
       </div>
     </>
   )
