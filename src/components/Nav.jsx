93dcade239a0e776df1b78fa7bbfa27b6f1da977
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-07-02 00:15:09 +0200

    Update button layout in Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,40 +24,38 @@ function Nav() {
             className="pl-5 pt-10 h-28"
           />
         </Link>
-
         <div className="gap-4 flex justify-end">
-          <Link to="/">
-            <button className=" w-24 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:border border-white">
-              Home
-            </button>
-          </Link>
-
           {isLoggedIn ? (
             <>
               <Link to="/listings">
-                <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:border border-white">
-                  My Kitchen
+                <button className=" w-32 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:border border-white">
+                  My Dish
                 </button>
               </Link>
               <Link to="/bookings">
-                <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:border border-white">
-                  Swap Request
+                <button className=" w-32 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:border border-white">
+                  Food Request
                 </button>
               </Link>
               <Link to="/profile">
-                <button className="p-2 h-10 items-center font-bold text-white text-xs md:text-base my-10  hover:border border-white">
+                <button className=" w-32 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:border border-white">
                   My Profile
                 </button>
               </Link>
               <button
                 onClick={logout}
-                className="h-10 items-center font-bold text-white text-xs md:text-base my-10 px-8  hover:border border-white"
+                className="w-32 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:border border-white"
               >
                 Log Out
               </button>
             </>
           ) : (
             <>
+              <Link to="/">
+                <button className=" w-24 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:border border-white">
+                  Home
+                </button>
+              </Link>
               <Link to="/login">
                 <button className=" w-24 h-10 items-center font-bold text-white text-xs md:text-base my-10 hover:border border-white">
                   Log In
