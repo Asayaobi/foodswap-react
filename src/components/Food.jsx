7ab61b1a9fe90555e92949a01057967c8ac50fef
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-07-04 18:25:14 +0200

    update gallery layout in food

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -34,25 +34,25 @@ function Food() {
         }}
       ></div>
       <Nav />
-      <div className="flex justify-center">
-        <div className="grid grid-cols-2 items-center place-items-center space-x-5 mx-40 my-28 ">
+      <div className="flex justify-center mx-72 my-24">
+        <div className="grid grid-cols-2 mx-36">
           {/* Gallery */}
-          <div className=" grid grid-rows-3 place-items-start">
+          <div className="grid grid-rows-3 place-items-start">
             <div className="grid row-span-2 aspect-square">
               <img
                 src={selectedImage}
                 alt="food pic"
-                className=" w-full h-full object-cover"
+                className="w-full h-full object-cover"
               />
             </div>
-            <div className=" grid grid-cols-3 gap-2 mt-3 ">
+            <div className="grid grid-cols-3 gap-2 mt-12">
               {food.images &&
                 food.images.slice(1).map((image, index) => (
                   <div key={index}>
                     <img
                       src={image}
                       alt="food pic"
-                      className=" w-full h-full object-cover cursor-pointer aspect-square"
+                      className="w-full h-full object-cover cursor-pointer aspect-square relative z-10"
                       onClick={(event) => {
                         setSelectedImage(image)
                       }}
@@ -62,8 +62,8 @@ function Food() {
             </div>
           </div>
           {/* Text */}
-          <div className="grid grid-rows-5 items-center ">
-            <div className="grid row-span-2 gap-4">
+          <div className="items-center ml-7">
+            <div className="gap-4">
               <div className=" text-4xl font-serif font-extrabold">
                 {food.food_title}
               </div>
@@ -73,7 +73,7 @@ function Food() {
               <div className=" pb-3">{food.description}</div>
               <hr />
             </div>
-            <div className="grid row-span-2 gap-4">
+            <div className="gap-4">
               <div className=" text-2xl font-serif font-extrabold">RATING</div>
               <div>
                 {[...Array(food.rating)].map((star, index) => (
