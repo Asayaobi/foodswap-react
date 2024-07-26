b62ac7c2c19d1429d1d0cd5b3df33ec013f9ba23
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-07-26 17:45:30 +0200

    update responsive layout to listings with comments

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -62,118 +62,122 @@ function Listings() {
 
   return (
     <>
-      <div className="relative h-screen">
-        <div className="absolute inset-0 bg-[url('https://preview.codeless.co/picante/default/wp-content/uploads/2018/07/home02_03-compressed.jpg')] bg-cover bg-center"></div>
-        <div className="absolute inset-0 flex md:items-center md:justify-center z-10">
-          <Nav />
-          <div className="bg-slate-100 p-8 opacity-90 rounded shadow-lg mx-2 max-w-4xl w-full mt-40 md:mt-0">
-            <h1 className="text-2xl font-bold mb-2 md:mb-8 font-serif">
-              My Dish
-            </h1>
-            <form onSubmit={(e) => submitForm(e)}>
-              <div className="grid md:grid-cols-3 md:gap-8">
-                <div>
-                  <div className="mb-4">
-                    <label className="block mb-2">Dish Name</label>
-                    <input
-                      type="text"
-                      name="food_title"
-                      className="border rounded w-full py-2 px-3"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block mb-2">Culinary Origin</label>
-                    <input
-                      type="text"
-                      name="country"
-                      className="border rounded w-full py-2 px-3"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block mb-2">Course Category</label>
-                    <select
-                      name="category"
-                      className="border rounded w-full py-2 px-3"
-                    >
-                      <option value="">Course Category</option>
-                      <option value="breakfast">Breakfast</option>
-                      <option value="appetizer">Appetizer</option>
-                      <option value="main dish">Main Dish</option>
-                      <option value="dessert">Dessert</option>
-                    </select>
-                  </div>
-                  <div className="mb-4">
-                    <label className="block mb-2">Ingredients</label>
-                    <input
-                      type="text"
-                      name="ingredients"
-                      className="border rounded w-full py-2 px-3"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block mb-2">Availability</label>
-                    <select
-                      name="available"
-                      className="border rounded w-full py-2 px-3"
-                    >
-                      <option value="true">Ready to swap</option>
-                      <option value="false">Not today</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="col-span-2">
-                  <label className="block mb-2">Description</label>
-                  <input
-                    type="text"
-                    name="description"
-                    className="border rounded py-2 px-3 w-full mb-4 pb-16 pr-16"
-                  />
-                  <label className="block mb-2 mt-2">Images</label>
-                  <input
-                    type="text"
-                    name="image"
-                    className="border rounded py-2 px-3 w-full mb-2"
-                  />
-                  <input
-                    type="text"
-                    name="image"
-                    className="border rounded py-2 px-3 w-full mb-2"
-                  />
-                  <input
-                    type="text"
-                    name="image"
-                    className="border rounded py-2 px-3 w-full mb-2"
-                  />
-                  <input
-                    type="text"
-                    name="image"
-                    className="border rounded py-2 px-3 w-full mb-2"
-                  />
-                  {errorMessage && (
-                    <div className="flex justify-start text-sm text-red-500">
-                      {errorMessage}
+      <div className="min-h-screen flex flex-col">
+        {/* Form Block */}
+        <div className="relative flex-grow">
+          <div className="absolute inset-0 bg-[url('https://preview.codeless.co/picante/default/wp-content/uploads/2018/07/home02_03-compressed.jpg')] bg-cover bg-center"></div>
+          <div className="relative z-10 flex flex-col md:flex-row md:items-center md:justify-center">
+            <Nav />
+            <div className="bg-slate-100 p-8 opacity-90 rounded shadow-lg mx-2 max-w-4xl w-full mt-40">
+              <h1 className="text-2xl font-bold mb-2 md:mb-8 font-serif">
+                My Dish
+              </h1>
+              <form onSubmit={(e) => submitForm(e)}>
+                <div className="grid md:grid-cols-3 md:gap-8">
+                  <div>
+                    <div className="mb-4">
+                      <label className="block mb-2">Dish Name</label>
+                      <input
+                        type="text"
+                        name="food_title"
+                        className="border rounded w-full py-2 px-3"
+                      />
                     </div>
-                  )}
-                  <div className="flex justify-end">
-                    <button className="bg-orange-500 hover:bg-orange-300 text-white mt-2 py-3 px-8 tracking-widest">
-                      ADD THIS DISH
-                    </button>
+                    <div className="mb-4">
+                      <label className="block mb-2">Culinary Origin</label>
+                      <input
+                        type="text"
+                        name="country"
+                        className="border rounded w-full py-2 px-3"
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label className="block mb-2">Course Category</label>
+                      <select
+                        name="category"
+                        className="border rounded w-full py-2 px-3"
+                      >
+                        <option value="">Course Category</option>
+                        <option value="breakfast">Breakfast</option>
+                        <option value="appetizer">Appetizer</option>
+                        <option value="main dish">Main Dish</option>
+                        <option value="dessert">Dessert</option>
+                      </select>
+                    </div>
+                    <div className="mb-4">
+                      <label className="block mb-2">Ingredients</label>
+                      <input
+                        type="text"
+                        name="ingredients"
+                        className="border rounded w-full py-2 px-3"
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label className="block mb-2">Availability</label>
+                      <select
+                        name="available"
+                        className="border rounded w-full py-2 px-3"
+                      >
+                        <option value="true">Ready to swap</option>
+                        <option value="false">Not today</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="col-span-2">
+                    <label className="block mb-2">Description</label>
+                    <input
+                      type="text"
+                      name="description"
+                      className="border rounded py-2 px-3 w-full mb-4 pb-16 pr-16"
+                    />
+                    <label className="block mb-2 mt-2">Images</label>
+                    <input
+                      type="text"
+                      name="image"
+                      className="border rounded py-2 px-3 w-full mb-2"
+                    />
+                    <input
+                      type="text"
+                      name="image"
+                      className="border rounded py-2 px-3 w-full mb-2"
+                    />
+                    <input
+                      type="text"
+                      name="image"
+                      className="border rounded py-2 px-3 w-full mb-2"
+                    />
+                    <input
+                      type="text"
+                      name="image"
+                      className="border rounded py-2 px-3 w-full mb-2"
+                    />
+                    {errorMessage && (
+                      <div className="flex justify-start text-sm text-red-500">
+                        {errorMessage}
+                      </div>
+                    )}
+                    <div className="flex justify-end">
+                      <button className="bg-orange-500 hover:bg-orange-300 text-white mt-2 py-3 px-8 tracking-widest">
+                        ADD THIS DISH
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-      {/* Cards Session */}
-      <div className="text-center bg-slate-100 py-4">
-        <div className="grid justify-stretch pb-28 pt-10 px-48">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-            {listings &&
-              listings.length > 0 &&
-              listings.map((food, index) => (
-                <Card key={index} food={food} isListing={true} />
-              ))}
+
+        {/* Cards Session */}
+        <div className="bg-slate-100 py-4">
+          <div className="text-center py-10 px-4 lg:px-48">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+              {listings &&
+                listings.length > 0 &&
+                listings.map((food, index) => (
+                  <Card key={index} food={food} isListing={true} />
+                ))}
+            </div>
           </div>
         </div>
       </div>
